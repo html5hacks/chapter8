4a672d81380f5bbb4e35f060a65ac6dfe428ea7c
Jesse Cravens <jesse.cravens@gmail.com>
2012-03-21 18:03:02 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes');
+
+var app = module.exports = express.createServer();
+
+// Configuration
+app.configure(function(){
+  app.use(express.static(__dirname + '/public'));
+
+  // disable layout
+  app.set("view options", {layout: false});
+
+  // make a custom html template
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler());
+});
+
+// Routes
+app.get('/', function(req, res){
+  res.render("index.html");
+});
+
+app.listen(process.env.PORT || 3000);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
