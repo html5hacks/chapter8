c104d6e41aa4ee574f7b1aec90322a6ff377df2e
Jesse Cravens <jesse.cravens@gmail.com>
2012-03-21 18:14:26 -0500

    added assets, views, and updated routes

--- app.js
+++ app.js
@@ -37,6 +37,30 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render("index.html");
 });
+app.get('/index.html', function(req, res){
+  res.render("index.html");
+});
+app.get('/scaffolding.html', function(req, res){
+  res.render("scaffolding.html");
+});
+app.get('/base-css.html', function(req, res){
+  res.render("base-css.html");
+});
+app.get('/components.html', function(req, res){
+  res.render("components.html");
+});
+app.get('/javascript.html', function(req, res){
+  res.render("javascript.html");
+});
+app.get('/less.html', function(req, res){
+  res.render("less.html");
+});
+app.get('/download.html', function(req, res){
+  res.render("download.html");
+});
+app.get('/examples.html', function(req, res){
+  res.render("examples.html");
+});
 
 app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
